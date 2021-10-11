2344cb929be5e7ca1408270e501cbe7cb29861b9
Beomsun <beomsun22@gmail.com>
2021-10-11 20:36:31 +0900

    ADD - querydsl설정 및 (상품, 카테고리 엔티티 추가), (상품, 카테고리 repository 추가 및 test)

--- src/test/java/com/manduljo/ohou/repository/category/ProductCategoryQueryRepositoryTest.java
+++ src/test/java/com/manduljo/ohou/repository/category/ProductCategoryQueryRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class ProductQueryRepositoryTest {
+class ProductCategoryQueryRepositoryTest {
 
     @Autowired
     private ProductCategoryRepository productCategoryRepository;
