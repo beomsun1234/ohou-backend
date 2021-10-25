f62e148ce2f19c1b7d8da49db391da8df6e28bee
Beomsun <beomsun22@gmail.com>
2021-10-25 18:11:19 +0900

    ADD - 요청한 ip 확인

--- src/main/java/com/manduljo/ohou/controller/HomeController.java
+++ src/main/java/com/manduljo/ohou/controller/HomeController.java
@@ -4,11 +4,14 @@ package com.manduljo.ohou.controller;
 
 import com.manduljo.ohou.oauth2.CustomUserDetails;
 import com.manduljo.ohou.service.ProductService;
+import com.manduljo.ohou.util.IpUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @RestController
@@ -16,9 +19,16 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class HomeController {
     private final ProductService productService;
+    private final IpUtil ipUtil;
     @GetMapping("/")
     public String home(){
         return "hello";
+    }
+
+
+    @GetMapping("/ip")
+    public String ip(HttpServletRequest httpServletRequest) throws Exception {
+        return ipUtil.getUserIp(httpServletRequest);
     }
 
     @GetMapping("/api/test")
---
+++ src/main/java/com/manduljo/ohou/util/IpUtil.java
@@ -0,0 +1,41 @@
+package com.manduljo.ohou.util;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class IpUtil {
+
+    public String getUserIp(HttpServletRequest request) throws Exception {
+        String ip = null;
+        ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-RealIP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("REMOTE_ADDR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        return ip;
+    }
+}
