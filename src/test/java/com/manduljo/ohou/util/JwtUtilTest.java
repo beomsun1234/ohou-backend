6ff5de1edbca65d8a1fb7061a1da4c2153de2c44
Beomsun <beomsun22@gmail.com>
2021-10-29 18:27:27 +0900

    EDIT - JwtUtil 단위 테스트

--- src/test/java/com/manduljo/ohou/util/JwtUtilTest.java
+++ src/test/java/com/manduljo/ohou/util/JwtUtilTest.java
@@ -1,53 +1,53 @@
 package com.manduljo.ohou.util;
 
-import com.manduljo.ohou.domain.member.LoginType;
+
 import com.manduljo.ohou.domain.member.Member;
-import com.manduljo.ohou.domain.member.Role;
-import com.manduljo.ohou.repository.member.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+
+@ExtendWith(MockitoExtension.class)
 class JwtUtilTest {
 
-    @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
+
+
     private JwtUtil jwtUtil;
 
-    @Transactional
-    @Rollback
     @BeforeEach
-    void member생성및저장(){
-        memberRepository.save(Member.builder().name("test").role(Role.ROLE_USER).email("test").loginType(LoginType.OHOU).build());
+    void init(){
+        jwtUtil = new JwtUtil("testtestsetsettestsetetestestestestestsettsetse");
     }
+
+
     
     @Test
-    @DisplayName("해당 유저 토큰생성 및 해당유저 email과 token에 email이 같은지 확인")
-    void test01(){
-
-        //when
-        Member member = memberRepository.findById(1L).orElseThrow();
+    @DisplayName("토큰생성 및 유저 email과 token에 email이 같은지 확인")
+    void test_generateToken(){
+        //given
+        Member member = Member.builder().email("test").name("test").build();
         String token = jwtUtil.generateToken(member.getEmail(), member.getName());
-
+        //when
+        String email = jwtUtil.getEmail(token);
         //then
-        Assertions.assertThat(member.getEmail()).isEqualTo(jwtUtil.getEmail(token));
+        Assertions.assertThat(email).isEqualTo(member.getEmail());
+        System.out.println("token="+token);
+
     }
 
     @Test
     @DisplayName("토큰 유효성 검사")
     void test2(){
-        //when
-        Member member = memberRepository.findById(1L).orElseThrow();
+        //given
+        Member member = Member.builder().email("test").name("test").build();
         String token = jwtUtil.generateToken(member.getEmail(), member.getName());
+        //when
+        Boolean validateToken = jwtUtil.validateToken(token, member);
         //then
-        Assertions.assertThat(jwtUtil.validateToken(token, member)).isTrue();
+        Assertions.assertThat(validateToken).isTrue() ;
     }
 
 
