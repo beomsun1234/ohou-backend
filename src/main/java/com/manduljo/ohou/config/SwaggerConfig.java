cd73b473fc0dc02bd3af11c7f5ecdacd4512d33f
daeho.oh <ohdaeho@gmail.com>
2021-11-18 19:55:29 +0900

    ADD - mongodb swagger 설정 수정

--- src/main/java/com/manduljo/ohou/config/SwaggerConfig.java
+++ src/main/java/com/manduljo/ohou/config/SwaggerConfig.java
@@ -28,7 +28,30 @@ public class SwaggerConfig {
                 .globalOperationParameters(global)
                 .select()
                 .apis(RequestHandlerSelectors.any()) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
-                .paths(PathSelectors.ant("/*api/**")) // 그중 /*api/** 인 URL들만 필터링
+                .paths(PathSelectors.ant("/api/**")) // 그중 /api/** 인 URL들만 필터링
+                .build();
+    }
+
+    @Bean
+    public Docket mongoApi() {
+        List<Parameter> global = new ArrayList<>();
+        global.add(
+            new ParameterBuilder()
+                .name("Authorization")
+                .description("Access Token ex)Bearer yourtoken")
+                .parameterType("header")
+                .required(false)
+                .modelRef(new ModelRef("string"))
+                .build()
+        );
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .globalOperationParameters(global)
+                .groupName("mongo")
+                .select()
+                .apis(RequestHandlerSelectors.any()) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
+                .paths(PathSelectors.ant("/mongo-api/**")) // 그중 /mongo-api/** 인 URL들만 필터링
                 .build();
     }
 }
