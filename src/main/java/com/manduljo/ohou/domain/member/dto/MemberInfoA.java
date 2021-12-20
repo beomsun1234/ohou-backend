70fe3fb857f3e662829f3368b83afa3f1f2c0fab
Beomsun <beomsun22@gmail.com>
2021-12-20 20:36:25 +0900

    ADD - 유저정보 조회(admin사용자에게 보여지는 정보)

---
+++ src/main/java/com/manduljo/ohou/domain/member/dto/MemberInfoA.java
@@ -0,0 +1,33 @@
+package com.manduljo.ohou.domain.member.dto;
+
+import com.manduljo.ohou.domain.member.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemberInfoA {
+    private Long id;
+    private String email;
+    private String nickName;
+    private String profileImage;
+    private String introduce;
+    private Gender gender;
+    private Role role;
+    private LoginType loginType;
+    private StatusAt statusAt;
+    @Builder
+    public MemberInfoA(Member entity){
+        this.id = entity.getId();
+        this.email=entity.getEmail();
+        this.nickName=entity.getName();
+        this.gender = entity.getGender();
+        this.profileImage = entity.getProfileImage();
+        this.introduce = entity.getIntroduce();
+        this.role = entity.getRole();
+        this.loginType  = entity.getLoginType();
+        this.statusAt = entity.getStatus_at();
+    }
+
+}
