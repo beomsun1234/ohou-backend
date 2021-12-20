8aa0fff3a83c645fda8985385d9da11523c8b331
Beomsun <beomsun22@gmail.com>
2021-12-20 20:39:57 +0900

    ADD - 로그인 시 유저 탈퇴 flag 확인 로직 추가

--- src/main/java/com/manduljo/ohou/service/AuthService.java
+++ src/main/java/com/manduljo/ohou/service/AuthService.java
@@ -4,6 +4,7 @@ import com.manduljo.ohou.domain.member.dto.MemberLoginRequestDto;
 import com.manduljo.ohou.oauth2.CustomUserDetails;
 import com.manduljo.ohou.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,6 +17,7 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtils;
@@ -25,6 +27,9 @@ public class AuthService {
                 new UsernamePasswordAuthenticationToken(memberLoginRequestDto.getEmail(), memberLoginRequestDto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        if (!userDetails.getMember().getStatus_at().isActive()){
+            throw new RuntimeException("탈퇴한 회원 입니다.");
+        }
         Map<String, Object> map = new HashMap<>();
         map.put("AccessToken",jwtUtils.generateToken(userDetails.getMember().getEmail(), userDetails.getMember().getName()));
         map.put("id",userDetails.getMember().getId());
