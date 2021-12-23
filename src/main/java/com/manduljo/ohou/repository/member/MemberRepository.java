8b006aa3f2aa0ebd6f1d379466e83db4481311a1
Beomsun <beomsun22@gmail.com>
2021-12-23 17:59:19 +0900

    EDIT - 어드민 유저 전체 조회시 탈퇴한 회원 보이지 않도록 수정

--- src/main/java/com/manduljo/ohou/repository/member/MemberRepository.java
+++ src/main/java/com/manduljo/ohou/repository/member/MemberRepository.java
@@ -1,10 +1,19 @@
 package com.manduljo.ohou.repository.member;
 
 import com.manduljo.ohou.domain.member.Member;
+import com.manduljo.ohou.domain.member.StatusAt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member>findByEmail(String email);
+
+    List<Member> findByStatusAt(StatusAt statusAt);
+
+    Optional<Member>findByEmailAndStatusAt(String email,StatusAt statusAt);
+
 }
--- src/main/java/com/manduljo/ohou/service/AdminService.java
+++ src/main/java/com/manduljo/ohou/service/AdminService.java
@@ -1,12 +1,12 @@
 package com.manduljo.ohou.service;
 
 import com.manduljo.ohou.domain.member.Member;
+import com.manduljo.ohou.domain.member.StatusAt;
 import com.manduljo.ohou.domain.member.dto.MemberInfoA;
 import com.manduljo.ohou.domain.product.dto.ProductInfo;
 import com.manduljo.ohou.repository.member.MemberRepository;
 import com.manduljo.ohou.repository.product.ProductQueryRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,12 +32,12 @@ public class AdminService {
     }
 
     /**
-     * 유저 전체조회
+     * 유저 전체 조회(탈퇴한 회원제외)
      * @return
      */
     @Transactional(readOnly = true)
     public List<MemberInfoA> findAll(){
-        return memberRepository.findAll().stream().map(member -> MemberInfoA.builder().entity(member).build()).collect(Collectors.toList());
+        return memberRepository.findByStatusAt(StatusAt.T).stream().map(member -> MemberInfoA.builder().entity(member).build()).collect(Collectors.toList());
     }
 
     /**
@@ -47,7 +47,7 @@ public class AdminService {
      */
     @Transactional(readOnly = true)
     public MemberInfoA findEmail(String email){
-        return MemberInfoA.builder().entity(memberRepository.findByEmail(email).orElseThrow(()-> new IllegalArgumentException("없는 사용자 입니다"))).build();
+        return MemberInfoA.builder().entity(memberRepository.findByEmailAndStatusAt(email,StatusAt.T).orElseThrow(()-> new IllegalArgumentException("없는 사용자 입니다"))).build();
     }
 
     /**
